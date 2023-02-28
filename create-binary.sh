d837722a9ddd1375ddaa3e8f1358a36dceb56e29
DeveloPolaroid <miguelttech.19@gmail.com>
2023-02-27 18:10:17 -0600

    Update create-binary.sh

--- create-binary.sh
+++ create-binary.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/sh
 
-sed -i '/PATH_SHELL_FILE/$1/' create_app.cpp
-g++ -g -Wall create_app.cpp -o $2
+# you need g++ installed
+# $1 is the path of sh file an example /opt/intellijIdea/bin/idea.sh
+# $2 the name of your binary file an example intellij 
+
+sed -i '/PATH_SHELL_FILE/$1/' template.cpp
+g++ -g -Wall template.cpp -o $2
 mv ${pwd}/${2} /usr/bin
+
+# Now call your binary from tour terminal as executable
